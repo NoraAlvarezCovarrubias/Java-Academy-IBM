d861bf659f44b7126bd7a3b555d173ea2c370a0d
hackerrank <63473788+noraglae@users.noreply.github.com>
2022-03-02 16:52:18 -0600

    Clase 01032022

--- demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
+++ demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
@@ -1,5 +1,7 @@
 package com.demo.steps.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.demo.steps.entities.Task;
@@ -8,36 +10,80 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskS1Controller {
+	
+	private final List<Task> taskList= new ArrayList<>();
 
-	@GetMapping("/all")
-	public ResponseEntity< List<Task> > fetchAllTask(){		
-        return null;
-        //ResponseEntity.ok().body("all tasks");
+	@GetMapping("")
+	public ResponseEntity< List<Task> > fetchAllTask(){
+		
+        return ResponseEntity.ok().body(taskList);
 	}
 
-	@PostMapping("/save")
-	public ResponseEntity<Task> postTask(Task newTask){
-		return null;
-        // ResponseEntity.status(HttpStatus.CREATED).body("task created");
+	@PostMapping("")
+	public ResponseEntity<Task> postTask(@RequestBody Task newTask){
+		newTask.setActive(false);
+		newTask.setCreateAt(LocalDateTime.now());
+		
+		long id = taskList.size()+1;
+		
+		newTask.setId(id);
+		taskList.add(newTask);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
 	}	
-
-	@DeleteMapping("/delete")
-	public ResponseEntity<Void> deleteTask( Long taskId){
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	
+	@GetMapping("/{taskId}")
+	public ResponseEntity< Task > fetchTaskById(@PathVariable("taskId") Long taskId){
+		
+		return taskList
+			.stream()
+			.filter( current -> taskId == current.getId() ) //Por cada elemento en la lista va a hacer la comparacion
+			.findFirst() //Cuando encuentra la primer coincidencia de la condicion de arriba (lo cual significa que no regresa null), hace la siguiente linea de map
+			.map(task -> ResponseEntity.ok().body(task) )
+			.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build()); //Quiere decir que regreso null en la condicion del filter
+		
+       // return ResponseEntity.ok().body(taskList);
 	}
 
-    @PutMapping("/update")
-	public ResponseEntity<Task>  putTask( Long TaskId, Task updatesTask){
-		return null;
+	@DeleteMapping("/{taskId}")
+	public ResponseEntity<?> deleteTask( @PathVariable("taskId") Long taskId){
+		return taskList
+				.stream()
+				.filter( current -> taskId == current.getId() )
+				.findFirst()
+				.map( task -> {
+					taskList.remove(task);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+		//return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+    @PutMapping("/{taskId}")
+	public ResponseEntity<Task>  putTask( @PathVariable("taskId") Long taskId, @RequestBody Task updatesTask){
+    	
+    	return taskList
+				.stream()
+				.filter( current -> taskId == current.getId() )
+				.findFirst()
+				.map(task -> {
+					task.setTitle( updatesTask.getTitle());
+					task.setDescription( updatesTask.getDescription());
+					task.setUpdatedAt(LocalDateTime.now());
+					return ResponseEntity.ok().body(task);
+				})
+				.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build());
         //ResponseEntity.ok().body("update");
 	}
 
--- demo-project/src/main/java/com/demo/steps/entities/Task.java
+++ demo-project/src/main/java/com/demo/steps/entities/Task.java
@@ -1,11 +1,16 @@
 package com.demo.steps.entities;
 
+import java.time.LocalDateTime;
+
 public class Task {
     
     private Long id;
     private String title;
     private String description;
-
+    private boolean active;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+    
     public Long getId() {
         return id;
     }
@@ -30,6 +35,28 @@ public class Task {
         this.description = description;
     }
 
+	public boolean isActive() {
+		return active;
+	}
 
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
     
 }
